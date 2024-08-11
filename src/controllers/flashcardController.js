38dd7bf553e5831d74e118ee3183e0304566ec90
pankil-soni <116267467+pankil-soni@users.noreply.github.com>
2024-08-11 18:20:38 +0530

    Added get routes for flashcards

--- src/controllers/flashcardController.js
+++ src/controllers/flashcardController.js
@@ -7,16 +7,53 @@ const createFlashcard = async (req, res) => {
     return res.status(400).json({ error: "Question and answer are required" });
   }
 
-  const flashcard = await prismaClient.flashcard.create({
-    data: {
-      question,
-      answer,
-      description,
-    },
-  });
-  res.status(200).json(flashcard);
+  try {
+    const flashcard = await prismaClient.flashcard.create({
+      data: {
+        question,
+        answer,
+        description,
+      },
+    });
+    res.status(200).json(flashcard);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getAllFlashcards = async (req, res) => {
+  try {
+    const flashcards = await prismaClient.flashcard.findMany();
+    res.json(flashcards);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getFlashcardById = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({ error: "Flashcard ID is required" });
+  }
+  try {
+    const flashcard = await prismaClient.flashcard.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!flashcard) {
+      return res.status(404).json({ error: "Flashcard not found" });
+    }
+
+    res.status(200).json(flashcard);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 module.exports = {
   createFlashcard,
+  getAllFlashcards,
+  getFlashcardById,
 };
--- src/routes/flashcard.js
+++ src/routes/flashcard.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { createFlashcard } = require("../controllers/flashcardController");
+const {
+  createFlashcard,
+  getAllFlashcards,
+  getFlashcardById,
+} = require("../controllers/flashcardController");
 
 const flashcardRouter = express.Router();
 
 flashcardRouter.post("/", createFlashcard);
+flashcardRouter.get("/", getAllFlashcards);
+flashcardRouter.get("/:id", getFlashcardById);
 
 module.exports = flashcardRouter;
